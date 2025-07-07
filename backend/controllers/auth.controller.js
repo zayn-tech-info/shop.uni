05d8a1abe4855d4bda8556a239b34ebcc3b24256
zayn-tech-info <zayntech.info@gmail.com>
2025-07-07 19:46:08 +0100

    protecting routes with a middleware

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,14 +5,12 @@ const sendToken = require("../utils/sendToken");
 
 const signup = asyncErrorHandler(async (req, res, next) => {
   const { username, fullname, email, password } = req.body;
-
   if (!username || !fullname || !email || !password) {
     const error = new customError("All fields are required", 400);
     return next(error);
   }
 
   const existingUser = await User.findOne({ email });
-
   if (existingUser) {
     const error = new customError("User already exist with this email", 400);
     return next(error);
@@ -24,14 +22,12 @@ const signup = asyncErrorHandler(async (req, res, next) => {
     email,
     password,
   });
+
   sendToken(user, res, "User registered successfully");
 });
 
-
-
 const login = asyncErrorHandler(async (req, res, next) => {
   const { email, password } = req.body;
-
   if (!email || !password) {
     const error = new customError("All fields are required", 404);
     next(error);
@@ -39,10 +35,8 @@ const login = asyncErrorHandler(async (req, res, next) => {
 
   const user = await User.findOne({ email }).select("+password");
   console.log(user);
-  
 
   const isPswrdMatch = await user.comparePswrd(password, user.password);
-
   if (!user || !isPswrdMatch) {
     const error = new customError("invalid email or password");
     return next(error);
@@ -51,4 +45,11 @@ const login = asyncErrorHandler(async (req, res, next) => {
   sendToken(user, res, "user logined succefully");
 });
 
-module.exports = { signup, login };
+const protectRoute = () => {
+  const testToken = req.header.authorization;
+  if (testToken && testToken.startWith("bearer")) {
+    const token = testToken.split(" ")[1];
+  }
+};
+
+module.exports = { signup, login, protectRoute };
